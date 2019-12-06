0f9768a5cc24b5de09b1ae81f63e10eea48f7701
rociojoo <rocio.joo@ufl.edu>
2019-12-06 13:11:50 -0500

    Adding dates table

--- ctv.R
+++ ctv.R
@@ -9,3 +9,12 @@ ctv2html(track, file = "Tracking.html")
 
 ## Check if everything's OK
 ctv::check_ctv_packages("Tracking.ctv")
+
+## Getting table with release dates
+library(kableExtra)
+info_pkgs <- read.csv("checks/RmovementPackagesInformation_checked.csv")
+info_pkgs_cran <- info_pkgs[!is.na(info_pkgs$recent_publish_data), c("Package","recent_publish_data")]
+names(info_pkgs_cran) <- c("Package", "Published date")
+rownames(info_pkgs_cran) <- NULL
+x <-  kable(info_pkgs_cran, format = "html", caption = "Published date of the examined CRAN packages")
+save_kable(kable_styling(x), file = "Pkg-Date.html", bs_theme = "simplex")
